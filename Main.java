9e68730f8cc25cb1ba7eaa316cbe2515b905cc60
nurtt0949 <nurtt0949@wrdsb.ca>
2020-09-12 04:35:19 +0000

    Finished the Hello World Problem

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 /**
- *
- * @author 
+ *This program prints Hello World to the screen
+ * @author Tolga Nurtekin 
  */
 public class Main {
 
@@ -9,7 +9,7 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // Prints "Hello Word" to the screen
+    System.out.println("Hello World!");
   }
 }
